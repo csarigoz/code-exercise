92b320d35d70a4a6f5f49b88cae43d7d6da73678
csarigoz <cagri.sarigoz@gmail.com>
2017-02-15 14:55:21 +0300

    Added fraction class

---
+++ pythonds/fraction_class.py
@@ -0,0 +1,65 @@
+def gcd(m,n):
+    while m%n != 0:
+        oldm = m
+        oldn = n
+
+        m = oldn
+        n = oldm%oldn
+    return n
+
+class Fraction:
+     def __init__(self,top,bottom):
+         self.num = top
+         self.den = bottom
+
+     def __str__(self):
+         return str(self.num)+"/"+str(self.den)
+
+     def show(self):
+         print(self.num,"/",self.den)
+
+     def __add__(self,other):
+         newnum = self.num*other.den + self.den*other.num
+         newden = self.den * other.den
+         common = gcd(newnum,newden)
+         return Fraction(newnum//common,newden//common)
+
+     def __eq__(self, other):
+         firstnum = self.num * other.den
+         secondnum = other.num * self.den
+         return firstnum == secondnum
+
+     def __mul__(self, other):
+         newnum = self.num * other.num
+         newden = self.den * other.den
+         common = gcd(newnum,newden)
+         return Fraction(newnum // common, newden // common)
+
+     def __truediv__(self, other):
+         newnum = self.num * other.den
+         newden = self.den * other.num
+         common = gcd(newnum,newden)
+         return Fraction(newnum // common, newden // common)
+
+     def __sub__(self, other):
+         newnum = self.num*other.den - self.den*other.num
+         newden = self.den * other.den
+         common = gcd(newnum,newden)
+         return Fraction(newnum//common,newden//common)
+
+     def __lt__(self, other):
+         return (self.num * other.den) < (other.num * self.den)
+
+     def __gt__(self, other):
+         return (self.num * other.den) > (other.num * self.den)
+
+
+x = Fraction(1,2)
+y = Fraction(2,3)
+print(x+y)
+print(x == y)
+print (x*y)
+print (x/y)
+print(x-y)
+print(x<y)
+print(x>y)
