05f4726b181a9b6a674b835c0a0a96bf483ae7cd
Cagri Sarigoz <cagri.sarigoz@gmail.com>
2017-03-24 19:37:52 +0300

    Add OrGate and NotGate subclasses

--- pythonds/LogicGate_class.py
+++ pythonds/LogicGate_class.py
@@ -1,6 +1,6 @@
 class LogicGate:
 
-    def __init__(selfself,n):
+    def __init__(self,n):
         self.label = n
         self.output = None
 
@@ -8,7 +8,7 @@ class LogicGate:
         return self.label
 
     def getOutput(self):
-        self.output = self.performGateLogin()
+        self.output = self.performGateLogic()
         return self.output
 
 class BinaryGate(LogicGate):
@@ -48,3 +48,30 @@ class AndGate(BinaryGate):
             return 1
         else:
             return 0
+
+class OrGate(BinaryGate):
+
+    def __init__(self,n):
+        BinaryGate.__init__(self,n)
+
+    def performGateLogic(self):
+
+        a = self.getPinA()
+        b = self.getPinB()
+        if a == 0 and b == 0:
+            return 0
+        else:
+            return 1
+
+class NotGate(UnaryGate):
+
+    def __init__(self,n):
+        UnaryGate.__init__(self,n)
+
+    def performGateLogic(self):
+
+        Pin = self.getPin()
+        if Pin == 1:
+            return 0
+        else:
+            return 1
